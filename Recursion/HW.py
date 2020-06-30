5d2c8fc8dbd3629a6d990340576d0ec58a4a9710
alaphu03 <phunguyen2004@gmail.com>
2020-06-30 22:45:34 +0700

    Enter

--- Recursion/HW.py
+++ Recursion/HW.py
@@ -16,14 +16,14 @@
 # def lt(x,n):
 #     if n == 1:
 #         return x
-#     if n != 1:
-#         return lt(x,n-1)*x
+#     return lt(x,n-1)*x
 # print(lt(2,5))
 
 arr = [2, 5, 8, 9, 2]
 counted = 0
 def count(arr,x,counted):
     if arr[0] == x :
-        return counted+1
+        counted+=1
+        return counted
     return count(arr[1:],x,counted) 
 print(count(arr,2,counted))
